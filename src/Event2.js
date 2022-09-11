6a6342fa18579e2cd3bd40aed9a97c3d07384c2a
kyoungyoon.yoon <kyoungyoon.yoon@metervision.io>
2022-09-11 15:48:50 +0900

    이벤트핸들링 컴포넌트 구현

---
+++ src/Event2.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const EventPractice = () =>
+{
+   const { name, setName } = useState('');
+   const { message, setMessage } = useState('');
+
+   const onNameChange = e => setName(e.target.value);
+   const onMessageChange = e => setMessage(e.target.value);
+
+   const onClick = () =>
+   {
+      alert(name + ' : ' + message);
+      setName('');
+      setMessage('');
+   };
+   const onKeyPress = (e) =>
+   {
+      if (e.key === 'Enter')
+      {
+         onClick();
+      }
+   }
+   return (
+      <div>
+         <h1>input함수</h1>
+         <input
+            type="text"
+            name="name"
+            placeholder="이름입력란"
+            value={name}
+            onChange={onNameChange}
+         />
+         <input
+            type="text"
+            name="message"
+            placeholder="메세지입력란"
+            value={message}
+            onChange={onMessageChange}
+            onKeyPress={onKeyPress}
+         />
+         <button
+            onClick={onClick}
+         >버튼</button>
+      </div>
+   )
+
+};
+export default EventPractice;
